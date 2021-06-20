e76e14c5c546f02914693fb9d12385f41239be29
Vadim Kasatkin <penguin2804@gmail.com>
2021-06-20 19:31:45 +0400

    install some package

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "index.js",
+  entry: "./src/index.js",
+  target: "web",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
@@ -11,22 +12,30 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public/index.html"),
+      template: path.resolve(__dirname, "src/pug/pages/index.pug"),
       title: "Blog",
+      filename: "index.html",
     }),
   ],
 
   devServer: {
-    contentBase: path.join(__dirname, "dist/public"),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
+    inline: true,
+    hot: true,
     port: 9000,
+    watchContentBase: true,
   },
 
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.s?css$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.pug$/i,
+        use: ["pug-loader"],
       },
     ],
   },
